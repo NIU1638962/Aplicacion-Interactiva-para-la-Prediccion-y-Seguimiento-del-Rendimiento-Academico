5d0f12ca1089059fd975dd9a3673908d8dd06a5c
Joel Tapia Salvador <91463596+NIU1638962@users.noreply.github.com>
2025-03-27 04:28:22 +0100

    Fix get_memory_system for all platforms

--- Code/utils.py
+++ Code/utils.py
@@ -29,8 +29,8 @@ def get_memory_cuda() -> Tuple[int, int, int]:
     """
     Get information about CUDA's device memory.
 
-    Print the memory information (total memory, free memory, used memory) in
-    MiB of the CUDA device being used and return such information in bytes.
+    Print the memory information (total memory, free memory, used memory) of
+    the CUDA device in human redable text and return such information in bytes.
 
     Returns
     -------
@@ -52,14 +52,14 @@ def get_memory_cuda() -> Tuple[int, int, int]:
 
     used_memory = total_memory - free_memory
 
-    free_memory_mib = free_memory / 2 ** 20
-    total_memory_mib = total_memory / 2 ** 20
-    used_memory_mib = used_memory / 2 ** 20
+    redable_free_memory = transform_redable_byte_scale(free_memory)
+    redable_total_memory = transform_redable_byte_scale(total_memory)
+    redable_used_memory = transform_redable_byte_scale(used_memory)
 
     verbose_memory_info_mib = (
-        f'Total CUDA Memory: {total_memory_mib}MiB'
-        + f'\nUsed CUDA Memory: {used_memory_mib}MiB'
-        + f'\nFree CUDA Memory: {free_memory_mib}MiB'
+        f'Total CUDA Memory: {redable_total_memory}'
+        + f'\nUsed CUDA Memory: {redable_used_memory}'
+        + f'\nFree CUDA Memory: {redable_free_memory}'
     )
 
     environment.logging.info(verbose_memory_info_mib.replace('\n', '\n\t\t'))
@@ -97,8 +97,8 @@ def get_memory_system() -> Tuple[int, int, int]:
     """
     Get information about system's memory.
 
-    Print the memory information (total memory, free memory, used memory) in
-    MiB of the system and return such information in bytes.
+    Print the memory information (total memory, free memory, used memory) of
+    the system in human redable text and return such information in bytes.
 
     Returns
     -------
@@ -110,21 +110,24 @@ def get_memory_system() -> Tuple[int, int, int]:
         Number of bytes currently free of the system.
 
     """
-    total_memory, free_memory, _, _, _ = environment.psutil.virtual_memory()
+    memory = environment.psutil.virtual_memory()
 
-    del _
+    total_memory = memory.total
+    free_memory = memory.available
+
+    del memory
     collect_memory()
 
     used_memory = total_memory - free_memory
 
-    free_memory_mib = free_memory / 2 ** 20
-    total_memory_mib = total_memory / 2 ** 20
-    used_memory_mib = used_memory / 2 ** 20
+    redable_free_memory = transform_redable_byte_scale(free_memory)
+    redable_total_memory = transform_redable_byte_scale(total_memory)
+    redable_used_memory = transform_redable_byte_scale(used_memory)
 
     verbose_memory_info_mib = (
-        f'Total System Memory: {total_memory_mib}MiB'
-        + f'\nUsed System Memory: {used_memory_mib}MiB'
-        + f'\nFree System Memory: {free_memory_mib}MiB'
+        f'Total System Memory: {redable_total_memory}'
+        + f'\nUsed System Memory: {redable_used_memory}'
+        + f'\nFree System Memory: {redable_free_memory}'
     )
 
     environment.logging.info(verbose_memory_info_mib.replace('\n', '\n\t\t'))
@@ -132,3 +135,27 @@ def get_memory_system() -> Tuple[int, int, int]:
     print(verbose_memory_info_mib)
 
     return total_memory, used_memory, free_memory
+
+
+def transform_redable_byte_scale(number_bytes: int) -> str:
+    """
+    Tranform a number of bytes into the apropiate unit of the scale to read it.
+
+    Parameters
+    ----------
+    number_bytes : Integer
+        Number of bytes to be transformed.
+
+    Returns
+    -------
+    String
+        Numebr of bytes in the most human redable unit of the scale.
+
+    """
+    scale_bytes = ('B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB')
+    i = 0
+    while number_bytes >= 2 ** 10:
+        number_bytes = number_bytes / (2 ** 10)
+        i += 1
+
+    return f'{number_bytes} {scale_bytes[i]}'
